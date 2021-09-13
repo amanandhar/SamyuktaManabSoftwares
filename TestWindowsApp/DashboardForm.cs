5460dddf5bf9503a67c1c1d6810f38957018159c
Adam Manandhar <Adam.Manandhar@mitchell.com>
2021-09-12 22:25:08 -0500

    Added new test project

---
+++ TestWindowsApp/DashboardForm.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestWindowsApp
+{
+    public partial class DashboardForm : Form
+    {
+        [DllImport("User32.dll", CharSet = CharSet.Auto)]
+        public static extern int ReleaseDC(IntPtr hWnd, IntPtr hDC);
+
+        [DllImport("User32.dll")]
+        private static extern IntPtr GetWindowDC(IntPtr hWnd);
+
+        protected override void WndProc(ref Message m)
+        {
+            base.WndProc(ref m);
+            const int WM_NCPAINT = 0x85;
+            if (m.Msg == WM_NCPAINT)
+            {
+                IntPtr hdc = GetWindowDC(m.HWnd);
+                if ((int)hdc != 0)
+                {
+                    Graphics g = Graphics.FromHdc(hdc);
+                    g.FillRectangle(Brushes.Green, new Rectangle(0, 0, 4800, 23));
+                    g.Flush();
+                    ReleaseDC(m.HWnd, hdc);
+                }
+            }
+        }
+
+        public DashboardForm()
+        {
+            InitializeComponent();
+            CustomizeDesign();
+        }
+
+        private void CustomizeDesign()
+        {
+            PanelMediaSubMenu.Visible = false;
+            PanelReports.Visible = false;
+            PanelSettings.Visible = false;
+        }
+
+        private void HideSubMenu()
+        {
+            if(PanelMediaSubMenu.Visible == true)
+            {
+                PanelMediaSubMenu.Visible = false;
+            }
+
+            if(PanelReports.Visible == true)
+            {
+                PanelReports.Visible = false;
+            }
+
+            if(PanelSettings.Visible == true)
+            {
+                PanelSettings.Visible = false;
+            }
+        }
+
+        private void ShowSubMenu(Panel panel)
+        {
+            if(panel.Visible == false)
+            {
+                HideSubMenu();
+                panel.Visible = true;
+            }
+            else
+            {
+                panel.Visible = false;
+            }
+        }
+
+        private void BtnMedia_Click(object sender, EventArgs e)
+        {
+            ShowSubMenu(PanelMediaSubMenu);
+        }
+
+        private void BtnMedia1_Click(object sender, EventArgs e)
+        {
+            HideSubMenu();
+        }
+
+        private void BtnMedia2_Click(object sender, EventArgs e)
+        {
+            HideSubMenu();
+        }
+
+        private void BtnMedia3_Click(object sender, EventArgs e)
+        {
+            HideSubMenu();
+        }
+
+        private void BtnMedia4_Click(object sender, EventArgs e)
+        {
+            HideSubMenu();
+        }
+
+        private void BtnReports_Click(object sender, EventArgs e)
+        {
+            ShowSubMenu(PanelReports);
+        }
+
+        private void BtnReports1_Click(object sender, EventArgs e)
+        {
+            OpenChildForm(new ReportsForm());
+        }
+
+        private void BtnReports2_Click(object sender, EventArgs e)
+        {
+            HideSubMenu();
+        }
+
+        private void BtnReports3_Click(object sender, EventArgs e)
+        {
+            HideSubMenu();
+        }
+
+        private void BtnReports4_Click(object sender, EventArgs e)
+        {
+            HideSubMenu();
+        }
+
+        private void BtnSettings_Click(object sender, EventArgs e)
+        {
+            ShowSubMenu(PanelSettings);
+        }
+
+        private void BtnSettings1_Click(object sender, EventArgs e)
+        {
+            OpenChildForm(new SettingsForm());
+        }
+
+        private void BtnSettings2_Click(object sender, EventArgs e)
+        {
+            HideSubMenu();
+        }
+
+        private void BtnSettings3_Click(object sender, EventArgs e)
+        {
+            HideSubMenu();
+        }
+
+        private void BtnSettings4_Click(object sender, EventArgs e)
+        {
+            HideSubMenu();
+        }
+
+        private Form activeForm = null;
+
+        private void OpenChildForm(Form childForm)
+        {
+            if(activeForm != null)
+            {
+                activeForm.Close(); ;
+            }
+
+            activeForm = childForm;
+            childForm.TopLevel = false;
+            childForm.FormBorderStyle = FormBorderStyle.None;
+            childForm.Dock = DockStyle.Fill;
+            PanelBody.Controls.Add(childForm);
+            PanelBody.Tag = childForm;
+            childForm.BringToFront();
+            childForm.Show();
+        }
+    }
+}
---
+++ TestWindowsApp/SettingsForm.Designer.cs
@@ -0,0 +1,140 @@
+﻿
+namespace TestWindowsApp
+{
+    partial class SettingsForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.BtnClose = new System.Windows.Forms.Button();
+            this.customButton1 = new TestWindowsApp.CustomControls.Button.CustomButton();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(88, 118);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(631, 352);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(437, 542);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Location = new System.Drawing.Point(542, 542);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button3.Location = new System.Drawing.Point(644, 542);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // BtnClose
+            // 
+            this.BtnClose.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.BtnClose.Location = new System.Drawing.Point(644, 51);
+            this.BtnClose.Name = "BtnClose";
+            this.BtnClose.Size = new System.Drawing.Size(75, 23);
+            this.BtnClose.TabIndex = 4;
+            this.BtnClose.Text = "Close";
+            this.BtnClose.UseVisualStyleBackColor = true;
+            this.BtnClose.Click += new System.EventHandler(this.BtnClose_Click);
+            // 
+            // customButton1
+            // 
+            this.customButton1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.customButton1.BackColor = System.Drawing.Color.MediumSlateBlue;
+            this.customButton1.BackgroundColor = System.Drawing.Color.MediumSlateBlue;
+            this.customButton1.BorderColor = System.Drawing.Color.PaleVioletRed;
+            this.customButton1.BorderRadius = 40;
+            this.customButton1.BorderSize = 0;
+            this.customButton1.FlatAppearance.BorderSize = 0;
+            this.customButton1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.customButton1.ForeColor = System.Drawing.Color.White;
+            this.customButton1.Location = new System.Drawing.Point(269, 533);
+            this.customButton1.Name = "customButton1";
+            this.customButton1.Size = new System.Drawing.Size(150, 40);
+            this.customButton1.TabIndex = 5;
+            this.customButton1.Text = "customButton1";
+            this.customButton1.TextColor = System.Drawing.Color.White;
+            this.customButton1.UseVisualStyleBackColor = false;
+            // 
+            // SettingsForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(814, 631);
+            this.Controls.Add(this.customButton1);
+            this.Controls.Add(this.BtnClose);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "SettingsForm";
+            this.Text = "PosForm";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button BtnClose;
+        private CustomControls.Button.CustomButton customButton1;
+    }
+}
