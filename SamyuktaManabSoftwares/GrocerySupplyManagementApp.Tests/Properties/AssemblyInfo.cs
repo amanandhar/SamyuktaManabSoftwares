50e54c9b7e1c1d14c71deeed2959ed5b43ff9adb
Adam Manandhar <adam.manandhar@gmail.com>
2021-10-21 10:26:02 -0500

    Added test project

---
+++ SamyuktaManabSoftwares/GrocerySupplyManagementApp.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("GrocerySupplyManagementApp.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("GrocerySupplyManagementApp.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("7f6b005b-2d8f-400f-8fda-3925a421cf15")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SamyuktaManabSoftwares/GrocerySupplyManagementApp.Tests/UnitTests/Services/BankServiceTest.cs
@@ -0,0 +1,97 @@
+﻿using GrocerySupplyManagementApp.Entities;
+using GrocerySupplyManagementApp.Repositories.Interfaces;
+using GrocerySupplyManagementApp.Services;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GrocerySupplyManagementApp.Tests.UnitTests.Services
+{
+    /// <summary>
+    /// Summary description for BankServiceTest
+    /// </summary>
+    [TestClass]
+    public class BankServiceTest
+    {
+        private Mock<IBankRepository> _bankRepository;
+        private BankService _sut;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            _bankRepository = new Mock<IBankRepository>();
+            _sut = new BankService(_bankRepository.Object);
+        }
+
+        [TestMethod]
+        [TestCategory("UnitTests"), TestCategory("Services.BankService")]
+        public void GetBanks_Returns_Banks()
+        {
+            _bankRepository.Setup(repo => repo.GetBanks())
+                .Returns(new List<Bank>() {
+                new Bank() { Id = 1, EndOfDay = "2078-01-01", Name = "Test Bank 1", AccountNo = "TestAccNo1" , AddedBy = "TestUser1", AddedDate = DateTime.Parse("2078-01-01"), UpdatedBy = null, UpdatedDate = null },
+                new Bank() { Id = 2, EndOfDay = "2078-02-02", Name = "Test Bank 2", AccountNo = "TestAccNo2" , AddedBy = "TestUser2", AddedDate = DateTime.Parse("2078-02-02"), UpdatedBy = null, UpdatedDate = null }
+            });
+
+            var banks = _sut.GetBanks();
+
+            Assert.AreEqual(2, banks.ToList().Count);
+        }
+
+        [TestMethod]
+        [TestCategory("UnitTests"), TestCategory("Services.BankService")]
+        public void GetBank_ReturnsBank_WhenNoError()
+        {
+            long id = 1;
+            _bankRepository.Setup(repo => repo.GetBank(It.IsAny<long>()))
+                .Returns(new Bank() { Id = 1, EndOfDay = "2078-01-01", Name = "Test Bank 1", AccountNo = "TestAccNo1", AddedBy = "TestUser1", AddedDate = DateTime.Parse("2078-01-01"), UpdatedBy = null, UpdatedDate = null });
+
+            var bank = _sut.GetBank(id);
+
+            Assert.IsNotNull(bank);
+            Assert.AreEqual(1, bank.Id);
+        }
+
+        [TestMethod]
+        [TestCategory("UnitTests"), TestCategory("Services.BankService")]
+        public void AddBank_ReturnsBank_WhenBankIsAdded()
+        {
+            _bankRepository.Setup(repo => repo.AddBank(It.IsAny<Bank>()))
+                .Returns(new Bank() { Id = 1, EndOfDay = "2078-01-01", Name = "Test Bank 1", AccountNo = "TestAccNo1", AddedBy = "TestUser1", AddedDate = DateTime.Parse("2078-01-01"), UpdatedBy = null, UpdatedDate = null });
+
+            var bank = _sut.AddBank(new Bank());
+
+            Assert.IsNotNull(bank);
+            Assert.AreEqual(1, bank.Id);
+        }
+
+        [TestMethod]
+        [TestCategory("UnitTests"), TestCategory("Services.BankService")]
+        public void UpdateBank_ReturnsBank_WhenBankIsUpdated()
+        {
+            long id = 1;
+            _bankRepository.Setup(repo => repo.UpdateBank(It.IsAny<long>(), It.IsAny<Bank>()))
+                .Returns(new Bank() { Id = 1, EndOfDay = "2078-01-01", Name = "Test Bank 1", AccountNo = "TestAccNo1", AddedBy = "TestUser1", AddedDate = DateTime.Parse("2078-01-01"), UpdatedBy = null, UpdatedDate = null });
+
+            var bank = _sut.UpdateBank(id, new Bank());
+
+            Assert.IsNotNull(bank);
+            Assert.AreEqual(1, bank.Id);
+        }
+
+        [TestMethod]
+        [TestCategory("UnitTests"), TestCategory("Services.BankService")]
+        public void DeleteBank_ReturnsTrue_WhenBankIsDeleted()
+        {
+            long id = 1;
+            _bankRepository.Setup(repo => repo.DeleteBank(It.IsAny<long>()))
+                .Returns(true);
+
+            var result = _sut.DeleteBank(id);
+
+            Assert.IsTrue(result);
+        }
+    }
+}
